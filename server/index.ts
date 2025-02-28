05fe56d720db6ad30ed3f2312516f0235c485a65
ItsBreathingjet <kyle.iakovidis@gmail.com>
2025-02-28 01:25:33 -0500

    Final web fix

--- server/index.ts
+++ server/index.ts
@@ -47,19 +47,15 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
   const port = 5000;
-  const host = process.platform === 'win32' ? 'localhost' : '0.0.0.0'; // Use localhost on Windows
+  const host = '0.0.0.0'; // Use 0.0.0.0 to allow access
+
   server.listen({
     port,
     host,
